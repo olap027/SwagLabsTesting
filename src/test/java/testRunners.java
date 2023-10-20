09bb8e65b93ecc2de77b0e060ce5afe57f3303fc
olala <olap27@gmail.com>
2023-10-20 17:33:56 +0800

    Revise codes in test runner

--- src/test/java/testRunners.java
+++ src/test/java/testRunners.java
@@ -4,9 +4,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/main/resources/",
-        plugin={"json:target/cucumber-report.json","html:target/cucumber-html",
-        "pretty","html:target/cucumber-reports.html"}
-        , monochrome = true,  tags = "@NotLogin and @TableListing"
-        )
+        plugin = {"json:target/cucumber-report.json", "html:target/cucumber-html",
+                "pretty", "html:target/cucumber-reports.html"},
+        monochrome = true,
+        tags = "@NotLogin or @TableListing"
+)
 public class testRunners {
 }
